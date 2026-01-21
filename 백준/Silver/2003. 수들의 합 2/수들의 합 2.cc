cf9484440c974e87c4d3658f6193241dac7d03ce
sanghyun003 <144121088+sanghyun003@users.noreply.github.com>
2026-01-21 20:01:07 +0900

    [Silver IV] Title: 수들의 합 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -3,36 +3,39 @@
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	int list[10000];
-	int N, M, num;
-	int answer = 0;
+    int left=0;
+    int right=0;
+    int num, N, M, sum=0, answer=0;
+    int list[10000] = { 0, };
 
-	cin >> N >> M;
+    cin >> N >> M;
 
-	for (int i = 0; i < N;i++) {
-		cin >> num;
-		list[i] = num;
-	}
-	for (int i = 0; i < N; i++) {
-		int s = list[i];
-		if (s == M) {
-			answer++;
-		}
-		else {
-			for (int j = i + 1; j < N; j++) {
-				s += list[j];
-				if (s == M) {
-					answer++;
-					break;
-				}
-			}
-		}
-	}
+    for (int i = 0; i < N; i++) {
+        cin >> num;
+        list[i] = num;
+    }
+    sum = list[0];
+    while (right <= N) {
+        if (sum < M) {
+            right++;
+            sum += list[right];
+        }
+        else if (sum == M) {
+            sum -= list[left];
+            left++;
+            answer++;
+            
+        }
+        else {
+            sum -= list[left];
+            left++;
+        }
+    }
 
-	cout << answer;
+    cout << answer;
 
-	return 0;
+    return 0;
 }
