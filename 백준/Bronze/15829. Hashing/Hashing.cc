d7ef5d0e691fef01286e30a842d1e5a0a6bc9ca2
sanghyun003 <144121088+sanghyun003@users.noreply.github.com>
2026-01-30 19:25:38 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int L, temp, H=0;
+	char c;
+	const int M = 1234567891;
+	int list[50];
+
+	cin >> L;
+
+	for (int i = 0; i < L; i++) {
+		cin >> c;
+		list[i] = c - 'a'+1;
+	}
+
+	for (int i = 0; i < L; i++) {
+		temp = list[i];
+		for (int j = 0; j < i; j++) {
+			temp *= 31;
+			temp %= M;
+		}
+		H += temp;
+		H %= M;
+	}
+
+	cout << H;
+
+	return 0;
+}
