06bd73f878b4db5665152b9279e6b8cff2e127bb
sanghyun003 <144121088+sanghyun003@users.noreply.github.com>
2026-02-06 18:49:58 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    
+    cin >> a >> b;
+    
+    do{
+        if( a%b==0) cout <<"multiple"<<'\n';
+        else if(b%a==0) cout << "factor" <<'\n';
+        else cout << "neither"<<'\n';
+        cin >> a >> b;
+    }
+    while(a!=0||b!=0);
+    
+    return 0;
+}
